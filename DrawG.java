243c76e186b4302aaa1227ee2698d4a8eef035b0
Ivan7077 <105739809+Ivan7077@users.noreply.github.com>
2022-05-31 19:10:12 -0400

    Add files via upload

--- DrawG.java
+++ DrawG.java
@@ -1,3 +1,4 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
@@ -11,7 +12,10 @@ import java.awt.event.MouseEvent;
 
 public class DrawG {
 	private JFrame frame;
-	
+	private JPanel panel;
+	private JButton button;
+
+	//Initial setup
 	 public DrawG() {
 	        frame = new JFrame("Chess Board");
 	        frame.setSize(650, 450);
@@ -19,21 +23,29 @@ public class DrawG {
 	        frame.setPreferredSize(frame.getSize());
 	        frame.add(new MultiDraw(frame.getSize()));
 	        frame.pack();
+	        panel = new JPanel();
+	        panel = new JPanel();
+	        button= new JButton("Click me");
+	        panel.add(button);
 	        frame.setVisible(true);
+	        panel.setVisible(true);
+	        
 	    }
-	 public static void main(String... argv) {
+	 	 public static void main(String... argv) {
 	        new DrawG();
+	       
 	    }
 	 public static class MultiDraw extends JPanel  implements MouseListener {
 	        int startX = 10;
 	        int startY = 10;
-	        int cellWidth = 50;
+	        int cellWidth = 60;
 	        int turn = 2;
 	        int rows = 6;
 	        int cols = 7;
-
+	        //Set details
 	        Color[][] grid = new Color[rows][cols];
 
+	//Draw grid
 	 public MultiDraw(Dimension dimension) {
          setSize(dimension);
          setPreferredSize(dimension);
@@ -47,10 +59,11 @@ public class DrawG {
      }
 
 	@Override
+	//paint background
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D)g;
         Dimension d = getSize();
-        g2.setColor(new Color(0, 0, 0));
+        g2.setColor(new Color(160,238,225));
         g2.fillRect(0,0,d.width,d.height);
         startX = 0;
         startY = 0;
@@ -70,8 +83,8 @@ public class DrawG {
 
         g2.setColor(new Color(255, 255, 255));
        
-            g2.drawString("pvp or pve",500,30);
-       
+            g2.drawString("pvp or pve",450,200);
+           
 
     }
 
@@ -79,7 +92,15 @@ public class DrawG {
 
         int x = e.getX();
         int y = e.getY();
-       // System.out.println(x + " " + y);
+        int xSpot = x/cellWidth;
+        int ySpot = y/cellWidth;
+        if(turn%2==0) {
+        	grid[ySpot][xSpot]= new Color(255,0,0);
+        }else {
+        	grid[ySpot][xSpot]=new Color(255,255,0);
+        }
+        System.out.println(xSpot + " " + ySpot);
+        //output where did user clicked on
         turn++;
         repaint();
     }
