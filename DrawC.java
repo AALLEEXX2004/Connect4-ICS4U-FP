a9e55b54b73a92804fd49d871620e14ba6bad1d3
Ivan7077 <105739809+Ivan7077@users.noreply.github.com>
2022-06-06 14:16:28 -0400

    Add files via upload

---
+++ DrawC.java
@@ -0,0 +1,72 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+
+import javax.swing.BorderFactory;
+
+public class DrawC {
+
+	private JFrame framein;
+	private JPanel panel;
+	private JButton button1;
+	private JButton button2;
+	private JButton button3;
+	private JButton button4;
+
+	
+	public DrawC(){
+	
+		framein = new JFrame("Connect Four");
+	 
+	    button1 = new JButton();
+	    button1.addActionListener(e -> System.out.println("Mode 0"));
+	    button1.setText("PvP");
+	    button1.setFocusable(false);
+	        
+	    button2 = new JButton();
+	    button2.addActionListener(e -> System.out.println("Mode 1"));
+	    button2.setText("PvE-Hard");
+	    button2.setFocusable(false);
+	    
+	    button3 = new JButton();
+	    button3.addActionListener(e -> System.out.println("Mode 2"));
+	    button3.setText("PvE-Medium");
+	    button3.setFocusable(false);
+
+	    button4 = new JButton();
+	    button4.addActionListener(e -> System.out.println("Mode 3"));
+	    button4.setText("PvE-Easy");
+	    button4.setFocusable(false);
+
+	        
+	 	
+	 	panel = new JPanel();
+	 	panel.setBorder(BorderFactory.createEmptyBorder(50,50,50,50));
+	 	panel.setLayout(new GridLayout(0,1));
+	 	panel.add(button1);
+	 	panel.add(button2);
+	 	panel.add(button3);
+	 	panel.add(button4);
+
+		
+	 	framein.add(panel, BorderLayout.CENTER);
+        framein.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        framein.pack();
+        framein.setVisible(true);
+        
+	 	
+        
+        
+        
+	 	
+	}
+
+	public static void main(String[] args) {
+		new DrawC();
+
+	}
+	
+}
